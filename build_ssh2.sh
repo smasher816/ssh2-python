a8bed4a19b1e7ea59f737885fada90c7d2e7cbdc
Pan <zuboci@yandex.com>
2017-08-15 20:55:24 +0100

    Updated libssh2 build script.

--- build_ssh2.sh
+++ build_ssh2.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -xe
 
-mkdir src
-cd src
+rm -rf src && mkdir src && cd src
 cmake -DBUILD_SHARED_LIBS=ON -DENABLE_ZLIB_COMPRESSION=ON \
       -DENABLE_CRYPT_NONE=ON -DENABLE_MAC_NONE=ON -DCMAKE_INSTALL_PREFIX=../lib \
       ../libssh2
 cmake --build .
+rm -rf ssh2/_libssh2 && mkdir -p ssh2/_libssh2
+cp -a src/src/*.so* ssh2/_libssh2/
